fce0ef13edc95bc06a3af0144bd6594c5be3c4eb
EliezerSunny <adetunjieliazer@gmail.com>
2024-06-26 17:53:05 -0700

    modified

--- models/review.py
+++ models/review.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""This is the review class"""
+"""
+Contains the class definition for Review
+"""
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
-
 class Review(BaseModel, Base):
-    """This is the class for Review
-    Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
-    """
-    __tablename__ = "reviews"
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    """This class defines a review by various attributes"""
+    __tablename__ = 'reviews'
+
     text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)

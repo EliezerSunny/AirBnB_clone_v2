4c9ee58ace80f954f423acbcd0890a53d01508de
EliezerSunny <adetunjieliazer@gmail.com>
2024-06-28 12:05:12 -0700

    modified

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,115 +1,46 @@
-#!/usr/bin/python3
-"""test for file storage."""
 import unittest
-import pep8
-import json
-import os
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 from models.engine.file_storage import FileStorage
-
+from models.state import State
 
 class TestFileStorage(unittest.TestCase):
-    '''this will test the FileStorage'''
+    """Test the FileStorage class"""
 
-    @classmethod
-    def setUpClass(cls):
-        """set up for test"""
-        cls.user = User()
-        cls.user.first_name = "Kev"
-        cls.user.last_name = "Yo"
-        cls.user.email = "1234@yahoo.com"
-        cls.storage = FileStorage()
-
-    @classmethod
-    def teardown(cls):
-        """at the end of the test this will tear it down"""
-        del cls.user
-
-    def tearDown(self):
-        """teardown"""
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
-
-    def test_pep8_FileStorage(self):
-        """Tests pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/engine/file_storage.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+    def setUp(self):
+        """Set up for the tests"""
+        self.storage = FileStorage()
+        self.storage._FileStorage__objects = {}
 
     def test_all(self):
-        """tests if all works in File Storage"""
-        storage = FileStorage()
-        obj = storage.all()
-        self.assertIsNotNone(obj)
-        self.assertEqual(type(obj), dict)
-        self.assertIs(obj, storage._FileStorage__objects)
-
-    def test_all_v2(self):
-        """Tests the new all() method in the v2"""
-        storage = FileStorage()
-        users = storage.all(User)
-        self.assertIsNotNone(users)
-        self.assertEqual(type(users), dict)
-
-    def test_new(self):
-        """test when new is created"""
-        storage = FileStorage()
-        obj = storage.all()
-        user = User()
-        user.id = 123455
-        user.name = "Kevin"
-        storage.new(user)
-        key = user.__class__.__name__ + "." + str(user.id)
-        self.assertIsNotNone(obj[key])
-
-    def test_reload_filestorage(self):
-        """Tests reload"""
+        """Test that all returns the correct dictionary of objects"""
+        state = State()
+        self.storage.new(state)
         self.storage.save()
-        Root = os.path.dirname(os.path.abspath("console.py"))
-        path = os.path.join(Root, "file.json")
-        with open(path, 'r') as f:
-            lines = f.readlines()
-        try:
-            os.remove(path)
-        except:
-            pass
+        all_objects = self.storage.all()
+        self.assertIn(f"State.{state.id}", all_objects)
+
+        # Test filtering by class
+        state2 = State()
+        self.storage.new(state2)
         self.storage.save()
-        with open(path, 'r') as f:
-            lines2 = f.readlines()
-        self.assertEqual(lines, lines2)
-        try:
-            os.remove(path)
-        except:
-            pass
-        with open(path, "w") as f:
-            f.write("{}")
-        with open(path, "r") as r:
-            for line in r:
-                self.assertEqual(line, "{}")
-        self.assertIs(self.storage.reload(), None)
+        all_states = self.storage.all(State)
+        self.assertEqual(len(all_states), 2)
+        self.assertIn(f"State.{state.id}", all_states)
+        self.assertIn(f"State.{state2.id}", all_states)
 
     def test_delete(self):
-        """Tests the delete v2 method"""
-        storage = FileStorage()
-        new_state = State()
-        new_state.name = "California"
-        storage.new(new_state)
-        storage.save()
-        states = storage.all(State)
-        self.assertIsNotNone(states)
-        self.assertEqual(type(states), dict)
-        storage.delete(new_state)
-        states = storage.all(State)
-        self.assertEqual(states, {})
+        """Test that delete removes an object from storage"""
+        state = State()
+        self.storage.new(state)
+        self.storage.save()
+        self.assertIn(f"State.{state.id}", self.storage.all())
+        self.storage.delete(state)
+        self.assertNotIn(f"State.{state.id}", self.storage.all())
 
+    def test_delete_none(self):
+        """Test that delete does nothing if obj is None"""
+        initial_objects = self.storage.all().copy()
+        self.storage.delete(None)
+        self.assertEqual(self.storage.all(), initial_objects)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()

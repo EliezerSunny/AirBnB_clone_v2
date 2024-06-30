ef355ddbe5f884957a21203a2840b307835ce96c
EliezerSunny <adetunjieliazer@gmail.com>
2024-06-30 13:07:05 -0700

    modified

---
+++ models/storage.py
@@ -0,0 +1,12 @@
+import os
+
+HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
+
+if HBNB_TYPE_STORAGE == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
+storage.reload()

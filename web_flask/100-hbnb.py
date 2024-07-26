06e84bdfde1fec405b5ca025df2a29232ff58928
EliezerSunny <adetunjieliazer@gmail.com>
2024-07-26 07:39:38 +0000

    modified

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -10,15 +10,16 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
     """Returns a rendered html template,
     using the web_static files
     """
     states = storage.all('State').values()
     cities = storage.all('City').values()
     amenities = storage.all('Amenity').values()
-    return render_template('10-hbnb_filters.html', **locals())
+    places = storage.all('Place').values()
+    return render_template('100-hbnb.html', **locals())
 
 
 @app.teardown_appcontext

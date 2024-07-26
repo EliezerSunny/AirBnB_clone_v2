efc2fffd386dbdbc97db6d1f3e8df625722d48b2
EliezerSunny <adetunjieliazer@gmail.com>
2024-07-26 09:42:36 +0000

    modified

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""Initializes the models package"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
+"""
+initialize the models package
+"""
+
 from os import getenv
 
-storage_type = getenv('HBNB_TYPE_STORAGE')
 
-if storage_type == 'db':
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
 storage.reload()
